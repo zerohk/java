146fb59c0d59311066a62efc4cc24f8badf535e4
Z3R0 <957442853@qq.com>
2018-03-01 05:45:37 +0800

    格式调好看一点

--- mashibing/TestArray/TestSort.java
+++ mashibing/TestArray/TestSort.java
@@ -8,20 +8,17 @@ public class TestSort
 		int length = a.length;
 		int j = 0;
 		int count = 0;
+    
 		do
 		{
-
 			temp[count] = a[0];
-			//int j = 0;
 			for(int i = 0;i < length;i++)
 			{
-
 				if(a[i] <= temp[count])
 				{
 					temp[count] = a[i];
 					j = i;
 				}
-
 			}
 			a[j] = a[length - 1];
 				count++;
